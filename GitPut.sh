b6e0edfd8e5ddd4647d1172562543ede1f7471ae
lmk114 <2039958658@qq.com>
2021-12-29 10:33:36 +0800

    final

---
+++ GitPut.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mv ~/*.sh ~/LinuxExam
+git add *.sh
+git commit -m "final"
+git push origin master
+
---
+++ TCstart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+~/apache-tomcat-8.5.72/bin/shutdown.sh
+~/apache-tomcat-8.5.72/bin/startup.sh
